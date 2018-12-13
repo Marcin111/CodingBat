0ae89d963535d7e38a970f46601244837ca0fac2
Marcin111 <marcin4412@gmail.com>
2018-12-13 22:21:39 +0100

    7

---
+++ src/codingbat/com/Array1/makePi.java
@@ -0,0 +1,14 @@
+package codingbat.com.Array1;
+
+public class makePi {
+
+//    Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+//
+//    makePi() → [3, 1, 4]
+
+    public int[] makePi() {
+        int [] array = new int[]{3,1,4};
+        return array;
+    }
+
+}
