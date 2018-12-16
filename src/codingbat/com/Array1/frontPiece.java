3f0ca15755045aab16a488c0ab6f98ba680df592
Marcin111 <marcin4412@gmail.com>
2018-12-16 23:34:03 +0100

    10

---
+++ src/codingbat/com/Array1/frontPiece.java
@@ -0,0 +1,21 @@
+package codingbat.com.Array1;
+
+public class frontPiece {
+
+//    Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+//
+//    frontPiece([1, 2, 3]) → [1, 2]
+//    frontPiece([1, 2]) → [1, 2]
+//    frontPiece([1]) → [1]
+
+    public int[] frontPiece(int[] nums) {
+
+        if (nums.length<=2){
+            return nums;
+        } else {
+            int[] array = new int[]{nums[0],nums[1]};
+            return array;
+        }
+    }
+
+}
