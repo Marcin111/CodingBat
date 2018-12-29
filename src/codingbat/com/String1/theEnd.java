21fccde77d287de404f2c9840644dd1acddff2fa
Marcin111 <marcin4412@gmail.com>
2018-12-29 20:54:43 +0100

    17

---
+++ src/codingbat/com/String1/theEnd.java
@@ -0,0 +1,19 @@
+package codingbat.com.String1;
+
+public class theEnd {
+
+//    Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+//
+//    theEnd("Hello", true) → "H"
+//    theEnd("Hello", false) → "o"
+//    theEnd("oh", true) → "o"
+
+    public char theEnd(String str, boolean front) {
+        if (front){
+            return str.charAt(0);
+        } else {
+            return str.charAt(str.length()-1);
+        }
+    }
+
+}
