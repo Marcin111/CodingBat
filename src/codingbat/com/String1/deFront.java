1bc8ca01749d4b047fb690893652cc0ffbb39344
Marcin111 <marcin4412@gmail.com>
2019-01-03 23:46:31 +0100

    22

---
+++ src/codingbat/com/String1/deFront.java
@@ -0,0 +1,24 @@
+package codingbat.com.String1;
+
+public class deFront {
+
+//    Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+//
+//    deFront("Hello") → "llo"
+//    deFront("java") → "va"
+//    deFront("away") → "aay"
+
+    public String deFront(String str) {
+        if (str.charAt(0)!='a' && str.charAt(1)!='b'){
+            return str.substring(2);
+        } else if ( str.charAt(0)=='a' && str.charAt(1)!='b'){
+            return "a"+ str.substring(2);
+        } else if ( str.charAt(0)!='a' && str.charAt(1)=='b'){
+            return "b"+ str.substring(2);
+        } else {
+            return str;
+        }
+    }
+
+
+}
