c54fdfe65f9e657fe214ba643ce8237179da752b
Marcin111 <marcin4412@gmail.com>
2019-01-06 21:28:15 +0100

    25

---
+++ src/codingbat/com/Logic1/caughtSpeeding.java
@@ -0,0 +1,24 @@
+package codingbat.com.Logic1;
+
+public class caughtSpeeding {
+
+//    You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+//
+//    caughtSpeeding(60, false) → 0
+//    caughtSpeeding(65, false) → 1
+//    caughtSpeeding(65, true) → 0
+
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        if (!isBirthday && speed<=60 || isBirthday && speed<=65){
+            return 0;
+        }
+        if (!isBirthday && speed<=80 && speed>=61 || isBirthday && speed<=85 && speed>=66){
+            return 1;
+        }
+        if (!isBirthday && speed>=81 || isBirthday && speed>=86){
+            return 2;
+        }
+        return 3;
+    }
+
+}
