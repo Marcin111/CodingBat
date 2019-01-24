7a9c11bb2858908a5cc4b67e3094f401152f1f81
Marcin111 <marcin4412@gmail.com>
2019-01-24 19:27:43 +0100

    40

---
+++ src/codingbat/com/Logic2/closeFar.java
@@ -0,0 +1,16 @@
+package codingbat.com.Logic2;
+
+public class closeFar {
+
+//    Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: Math.abs(num) computes the absolute value of a number.
+//
+//    closeFar(1, 2, 10) → true
+//    closeFar(1, 2, 3) → false
+//    closeFar(4, 1, 3) → true
+
+    public boolean closeFar(int a, int b, int c) {
+        return (Math.abs(b-a)<=1 && Math.abs(a-c)>=2 && Math.abs(b-c)>=2 ||
+                Math.abs(c-a)<=1 && Math.abs(a-b)>=2 && Math.abs(b-c)>=2);
+    }
+
+}
