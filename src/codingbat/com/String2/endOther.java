1c444b27ed09edf9edbf46eefe5c84dc0ff863be
Marcin111 <marcin4412@gmail.com>
2019-01-28 23:55:36 +0100

    43

---
+++ src/codingbat/com/String2/endOther.java
@@ -0,0 +1,17 @@
+package codingbat.com.String2;
+
+public class endOther {
+
+//    Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+//
+//    endOther("Hiabc", "abc") → true
+//    endOther("AbC", "HiaBc") → true
+//    endOther("abc", "abXabc") → true
+
+    public boolean endOther(String a, String b) {
+        String alc = a.toLowerCase();
+        String blc = b.toLowerCase();
+        return alc.endsWith(blc) || blc.endsWith(alc);
+    }
+
+}
