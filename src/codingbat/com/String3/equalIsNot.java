8896c29a0165eb1fcf29dd5635bd210cfbeccb82
Marcin111 <marcin4412@gmail.com>
2019-02-11 13:49:16 +0100

    56

---
+++ src/codingbat/com/String3/equalIsNot.java
@@ -0,0 +1,25 @@
+package codingbat.com.String3;
+
+public class equalIsNot {
+
+//    Given a string, return true if the number of appearances of "is" anywhere in the string is equal to the number of appearances of "not" anywhere in the string (case sensitive).
+//
+//    equalIsNot("This is not") → false
+//    equalIsNot("This is notnot") → true
+//    equalIsNot("noisxxnotyynotxisi") → true
+
+    public boolean equalIsNot(String str) {
+        int countIs = 0;
+        int countNot = 0;
+        for (int i = 0; i<str.length()-1; i++){
+            if (str.substring(i,i+2).equals("is")){
+                countIs++;
+            }
+            if (i<str.length()-2 && str.substring(i,i+3).equals("not")){
+                countNot++;
+            }
+        }
+        return countIs==countNot;
+    }
+
+}
