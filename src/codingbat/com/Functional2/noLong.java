146c6ef6a06744a6eead7e9b2a8fc5742b4cdc8e
Marcin111 <marcin4412@gmail.com>
2019-02-25 20:29:03 +0100

    69

---
+++ src/codingbat/com/Functional2/noLong.java
@@ -0,0 +1,18 @@
+package codingbat.com.Functional2;
+
+import java.util.List;
+
+public class noLong {
+
+//    Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+//
+//    noLong(["this", "not", "too", "long"]) → ["not", "too"]
+//    noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+//    noLong(["cccc", "cccc", "cccc"]) → []
+
+    public List<String> noLong(List<String> strings) {
+        strings.removeIf(n->n.length()>=4);
+        return strings;
+    }
+
+}
