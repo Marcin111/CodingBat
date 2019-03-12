f87366729d5b9940a49706de2e909b19d482dbf6
Marcin111 <marcin4412@gmail.com>
2019-03-12 22:56:20 +0100

    84

---
+++ src/codingbat/com/AP1/scores100.java
@@ -0,0 +1,18 @@
+package codingbat.com.AP1;
+
+public class scores100 {
+
+//    Given an array of scores, return true if there are scores of 100 next to each other in the array. The array length will be at least 2.
+//
+//    scores100([1, 100, 100]) → true
+//    scores100([1, 100, 99, 100]) → false
+//    scores100([100, 1, 100, 100]) → true
+
+    public boolean scores100(int[] scores) {
+        for(int i = 0; i <= scores.length - 2; i++) {
+            if(scores[i] == 100 && scores[i+1] == 100)
+                return true;
+        }
+        return false;
+    }
+}
