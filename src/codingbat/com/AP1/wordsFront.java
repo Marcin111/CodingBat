5042f506f5c2db8ea3c03ec9798bf298a2aebc99
Marcin111 <marcin4412@gmail.com>
2019-03-17 22:48:12 +0100

    88

---
+++ src/codingbat/com/AP1/wordsFront.java
@@ -0,0 +1,18 @@
+package codingbat.com.AP1;
+
+public class wordsFront {
+
+//    Given an array of strings, return a new array containing the first N strings. N will be in the range 1..length.
+//
+//    wordsFront(["a", "b", "c", "d"], 1) → ["a"]
+//    wordsFront(["a", "b", "c", "d"], 2) → ["a", "b"]
+//    wordsFront(["a", "b", "c", "d"], 3) → ["a", "b", "c"]
+
+    public String[] wordsFront(String[] words, int n) {
+        String[] arr = new String[n];
+        for(int i = 0; i < arr.length; i++)
+            arr[i] = words[i];
+        return arr;
+    }
+
+}
