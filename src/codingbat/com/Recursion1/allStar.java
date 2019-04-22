8810ffb496eda3db567cc21b42b7f200213759f9
Marcin111 <marcin4412@gmail.com>
2019-04-22 19:30:54 +0200

    121

---
+++ src/codingbat/com/Recursion1/allStar.java
@@ -0,0 +1,17 @@
+package codingbat.com.Recursion1;
+
+public class allStar {
+
+//    Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+//
+//    allStar("hello") → "h*e*l*l*o"
+//    allStar("abc") → "a*b*c"
+//    allStar("ab") → "a*b"
+
+    public String allStar(String str) {
+        if(str.length() <= 1)
+            return str;
+        return str.charAt(0) + "*" + allStar(str.substring(1));
+    }
+
+}
